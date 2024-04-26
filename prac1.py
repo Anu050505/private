43e25aa4de606a664603e3f75f7a8d2b29fcc736
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-26 16:08:45 +0800

    prac1

---
+++ prac1.py
@@ -0,0 +1,21 @@
+response='''How are you miss maam?
+i hope your fine or maybe not...
+me too i am great
+do you want to talk or not?
+no bye out'''                       #triple quotation prints everthing even if its in the next line
+
+
+print(response) 
+print(len(response))
+print(response[0])
+print(response[-1])
+
+for character in response:
+    print(character)
+
+
+
+
+
+
+
