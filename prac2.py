58b7de97c1a7b411c6589e7f2374adbe3c8d57c1
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-26 16:34:47 +0800

    Prac2

---
+++ prac2.py
@@ -0,0 +1,16 @@
+name='Anushka Bist'
+print(name[0:7])     #start from 0 and end with the idx+1 to get the last value
+print(len(name))
+
+len1=len(name)
+print('Names are usually', len1, 'words long')
+
+
+
+fruit='Mango'
+print(fruit[:4])
+print(fruit[:4])
+print(fruit[1:5])    #includes 1 but not 5
+print(fruit[1:])
+print(fruit[-2:-1]) #includes -2 but not -1
+print(fruit[-1])
