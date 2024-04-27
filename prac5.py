4fa9bb1ac750196ddedc8355ef40ef074d635495
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-28 00:49:33 +0800

    prac5

---
+++ prac5.py
@@ -0,0 +1,49 @@
+#break statement--------------------------------
+
+for i in range(12):
+    print('5 x',i+1,'=',(i+1)*5)
+    if(i==10):
+        break  #get out of the loop
+
+print('stop the loop')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
