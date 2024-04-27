dd70b5ae72db5b82271fa79bc5cf767076ca4dfd
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-28 00:32:46 +0800

    prac4

--- prac4.py
+++ prac4.py
@@ -1,14 +1,92 @@
-time=int(input("Enter the time according to 24hr clock: "))
-print(time)
+# IF ELSE FUNCTION---------------------------------------------
 
-if(time>=0000 and time<1200):
-    print("Good morning!")
+#time=int(input("Enter the time according to 24hr clock: "))
+#print(time)
 
-elif(time>=1200 and time<=1600):
-    print('Good afternoon!')
+#if(time>=0000 and time<1200):
+    #print("Good morning!")
 
-elif(time>1600 and time<=2400):
-    print('Good night!')
+#elif(time>=1200 and time<=1600):
+    #print('Good afternoon!')
 
+#elif(time>1600 and time<=2400):
+    #print('Good night!')
+
+#else:
+    #print('ERROR!')
+
+
+
+
+
+'''FOR FUNCTION---------------------------------------------------
+
+name='anushka'
+for i in name:
+    print(i, end=' ')  # end=' ' makes the value write with spacings in between
+    print(i, end=',')   # end=',' makes the value write with comma seperator
+    print(i)    #prints one by one in vertical format
+
+
+
+colour=['red','blue','black','white','green']
+for color in colour:
+    print(color)     # prints each element one by one
+    for i in color:
+        print(i)     #further prints each letter in each element'''
+
+
+
+
+
+#RANGE FUNCTION-------------------------------------------------
+
+#for i in range(5):
+    #print(i)        # prints from 0 to 4 
+    #print(i+1)       # prints from 1 to 5 (beacuse k+1)
+
+
+#for i in range(1,9):
+    #print(i)         # prints 1 to 8
+
+
+#range(start,stop,step)      step basically increases start number by the given step number till the stop number
+
+
+
+
+
+'''WHILE LOOP-----------------------------------------------------------
+
+i=0
+while(i<3):
+    print(i)
+    i+=1'''
+
+
+i=5
+while(i>0):
+    print(i)
+    i-=1
 else:
-    print('ERROR!')
+    print("I am done with the loop")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
