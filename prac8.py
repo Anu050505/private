9e8a04b81fc2625c91e149f467130f2a1e3fe8fd
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-28 23:45:24 +0800

    prac8

---
+++ prac8.py
@@ -0,0 +1,35 @@
+tup1=(1,)                  # In tuple always have comma in the end when there is only 1 element
+print(type(tup1),tup1)
+tup2=(1,2,3,4,5,6,7,1)
+print(type(tup2),tup2)
+
+tup2=(12,67,45,60,32,13,4)
+print(tup2[0])
+print(tup2[2])
+print(tup2[3])
+print(tup2[5])
+
+if 67 in tup2:
+    print('YES')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
