a0096424c5c26b7c353e8f0d3f0619cf34fb3f51
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-28 14:29:52 +0800

    prac5

--- prac5.py
+++ prac5.py
@@ -1,18 +1,29 @@
 #break statement--------------------------------
 
+#for i in range(12):
+    #print('5 x',i+1,'=',(i+1)*5)
+
+#print('stop the loop')
+
+
+
+#for i in range(12):
+    #print('5 x',i,'=',(i))
+    #if(i==10):
+        #break  #get out of the loop
+
+#print('stop the loop')
+
+
+
+
 for i in range(12):
-    print('5 x',i+1,'=',(i+1)*5)
+    print('5 x',i,'=',(i))
     if(i==10):
-        break  #get out of the loop
+        print('Skip the iteration')    
+        continue
 
 print('stop the loop')
-
-
-
-
-
-
-
 
 
 
