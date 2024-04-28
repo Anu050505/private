8c72227cfd385dcf2df59d1e0b2098599eb1e23d
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-28 18:40:51 +0800

    prac6

---
+++ prac6.py
@@ -0,0 +1,64 @@
+#def average(a,b):
+    #print('the average is', (a+b)/2)
+
+#average(7,3)
+
+
+
+
+
+#LIST-----------------------------------------------------------
+
+'''list=[1,2,3,4,5,6,7,8]
+
+print(list[0])
+print(list[-1])
+
+print(list[-2])          #both have the same meaning of code
+print(len(list)-1)
+
+
+if 5 in list:           # "if....in..." means checking if that element is in the list or not
+    print('YES')
+else:
+    print('NO')'''
+
+
+
+#list1=[1,2,4,6,'Anushka',True,7,8,9,4]
+#if True in list1:
+    #print('Hehe')
+#else:
+    #print('Haha')
+
+#if 'nush' in 'nushka':
+    #print('YES')
+#else:
+    #print('NO')
+
+
+
+'''print(list1)
+print(list1[1:-1])
+print(list1[0:10:2])           #slicing
+print(list1[0:7:3])'''
+
+
+
+lst=[i for i in range(101)]   # basically from 0 to 100 is stored in i and lst=i
+print(lst)
+
+lst=[i for i in range(101) if i%2==0]
+print(lst)
+
+
+
+
+
+
+
+
+
+
+
+
