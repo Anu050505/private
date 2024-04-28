02e8aeaa7572129545b46e64ac9f5db7fcf3e643
Anush <anushka.23@ichat.sp.edu.sg>
2024-04-28 20:57:02 +0800

    prac7

--- prac7.py
+++ prac7.py
@@ -1,18 +1,30 @@
 
+'''list=[21,2,13,54,44,5,6,27,18]
+print(list)
+list.append('Anushka')     # you have to initialise and then print 
+print(list)                # adds a element to the end of the list
 
-list=[21,2,13,54,44,5,6,27,18]
-print(list.append(123))
-l=list.append(100)
+                            # the list has been altered and includes new variables
+
+list.append(123)            
+print(list)
+list.sort()
+print(list)                 # this sorts the list in increasing order
+
+list.sort(reverse=True)     # this sorts the list in decreasing order
+print(list)
+
+print(list.index(2))        # gives the index of the element in the list
+
+print(list.count(1))        # counts the number of times that element appears in the list
+
+list.insert(1,100)          # adds a element in the given index "(idx,element)"
+print(list)'''
+
+l=[1,2,3,4,5,6,7,8]
+m=[100,190,180]
+l.extend(m)
 print(l)
-
-
-
-
-
-
-
-
-
 
 
 
